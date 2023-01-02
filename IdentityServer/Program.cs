5e5eeb1c756704b91ebcb337c17fde4bec2960fc
omkarsabale099 <omkarsabale099@gmail.com>
2023-01-03 00:04:45 +0530

    IdentityServer First Commit

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,34 @@
+using IdentityServer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddIdentityServer()
+        .AddInMemoryClients(IdentityConfiguration.Clients)
+        .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+        .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+        .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+        .AddTestUsers(IdentityConfiguration.TestUsers)
+        .AddDeveloperSigningCredential();
+
+
+
+        var app = builder.Build();
+
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+
+        app.Run();
+    }
+}
